ca6c0a8d711bd14b0c339bfbd4cdb08317cf01a6
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-04-10 07:52:25 +0200

    Refactor(): Remove current codebase.
    The current code base will be removed for the large refactor to WXT.
    We are going to use WXT in combination with logic from Hypothes.

--- src/lib/get-current-date.ts
+++
@@ -1,8 +0,0 @@
-const getCurrentDate = (stamp: boolean = true) => {
-  const date = new Date();
-  if (!stamp) {
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-  }
-  return date.getTime().toString();
-};
-export default getCurrentDate
--- src/lib/get-resource-url.ts
+++
@@ -1,6 +0,0 @@
-const getResourceURL = () => {
-    const url = window.location.href;
-    return url;
-};
-
-export default getResourceURL;
