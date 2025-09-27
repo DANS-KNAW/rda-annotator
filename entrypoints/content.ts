cf30a0646f5dcab5f8ee01e4b5b5e71d6cc291e1
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-09-27 11:06:21 +0200

    feat(sidebar): inject sidebar frame
    The commit removes some boilerplate files as well as setup the initial
    injection for the sidebar.

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,58 @@
+const EXTENSION_NAME = "rda-annotator";
+const SIDEBAR_WIDTH = 400;
+
+// We use rem unit for more conisistent sizing across different browsers.
+// This assumes the default font size is 16px.
+const widthRem = SIDEBAR_WIDTH / 16;
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["*://*/*"],
+  async main(ctx) {
+    const sidebarShadow = await createShadowRootUi(ctx, {
+      name: `${EXTENSION_NAME}-sidebar`,
+      position: "inline",
+      anchor: "body",
+      mode: "closed",
+
+      onMount(_, shadowRoot, shadowHost) {
+        const div = document.createElement("div");
+        shadowRoot.replaceChildren(div);
+
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(`
+          :host {
+            position: fixed;
+            top: 0; 
+            right: 0;
+            z-index: 2147483647;
+
+            width: ${widthRem}rem;
+            height: 100vh;
+            
+            box-sizing: border-box;
+          }
+        `);
+        shadowRoot.adoptedStyleSheets = [sheet];
+
+        const shadowFrame = createIframeUi(ctx, {
+          page: "/sidebar.html",
+          position: "inline",
+          anchor: div,
+
+          onMount(wrapper, iframe) {
+            wrapper.style.width = `${widthRem}rem`;
+            wrapper.style.height = "100vh";
+
+            iframe.width = "100%";
+            iframe.height = "100%";
+            iframe.style.border = "none";
+          },
+        });
+
+        shadowFrame.mount();
+      },
+    });
+
+    sidebarShadow.mount();
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: {
+    web_accessible_resources: [
+      { resources: ["sidebar.html"], matches: ["<all_urls>"] },
+    ],
+  },
 });
