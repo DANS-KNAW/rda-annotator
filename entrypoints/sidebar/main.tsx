87315ae17e7edef5ee02225dfb4ecf5b7b651fb8
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-09-27 12:26:51 +0200

    feat(): setup routing

--- entrypoints/sidebar/main.tsx
+++ entrypoints/sidebar/main.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-ReactDOM.createRoot(document.getElementById('sidebar')!).render(
+ReactDOM.createRoot(document.getElementById("sidebar")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter basename="/sidebar.html">
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
