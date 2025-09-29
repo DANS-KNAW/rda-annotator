3ba1af620045be84260654d1b4cb7556d9b36d3b
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-09-29 07:13:16 +0200

    feat(): add faq and accordion component

---
+++ components/Accordion.tsx
@@ -0,0 +1,54 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+
+interface AccordionProps {
+  question: string;
+  answer: string | React.ReactNode;
+}
+
+export default function Accordion({ question, answer }: AccordionProps) {
+  return (
+    <Disclosure as="div" className="py-4 first:pt-0 last:pb-0">
+      <dt>
+        <DisclosureButton className="group flex w-full items-center justify-between text-left text-gray-900">
+          <span className="text-sm">{question}</span>
+          <span className="ml-6 flex h-7 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              aria-hidden="true"
+              className="size-5 group-data-open:hidden"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              aria-hidden="true"
+              className="size-6 group-not-data-open:hidden"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+            </svg>
+          </span>
+        </DisclosureButton>
+      </dt>
+      <DisclosurePanel as="dd" className="mt-2">
+        <p className="text-sm/6 text-gray-600">{answer}</p>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
---
+++ components/FAQ.tsx
@@ -0,0 +1,39 @@
+import Accordion from "./Accordion";
+
+const faqs = [
+  {
+    question: "Where can I find my annotations?",
+    answer: (
+      <>
+        Your annotations will currently be deposited in the RDA Graph, and are
+        available from the{" "}
+        <a
+          className="text-rda-500 underline"
+          target="_blank"
+          href="https://www.rd-alliance.org"
+        >
+          RDA Discovery application
+        </a>
+      </>
+    ),
+  },
+];
+
+export default function FAQ() {
+  return (
+    <div className="px-2 py-8">
+      <h2 className="text-xl font-semibold tracking-tight text-gray-900">
+        Frequently asked questions
+      </h2>
+      <dl className="mt-6 divide-y divide-gray-900/10">
+        {faqs.map((faq) => (
+          <Accordion
+            key={faq.question}
+            question={faq.question}
+            answer={faq.answer}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+}
