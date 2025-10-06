46e8b4a4ff618d92fea7ce6b282d5745de7efa65
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-10-06 12:58:53 +0200

    bug(): add proper image loading

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,6 +1,6 @@
-import tiger from "@/assets/tiger.png";
-
 export function Topbar() {
+  const tigerUrl = browser.runtime.getURL("/tiger.png");
+
   return (
     <div className="w-full h-[5.5rem] relative px-4 bg-gradient-to-br from-rda-500 from-20% via-rda-600 via-60% to-brown-500 to-100%">
       <div className="flex justify-between items-center pt-2">
@@ -10,7 +10,7 @@ export function Topbar() {
         </h1>
         <div className="relative h-20 w-32">
           <img
-            src={tiger}
+            src={tigerUrl}
             alt="RDA-TIGER"
             className="absolute h-tiger -bottom-2 z-50"
           />
